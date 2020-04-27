549b177f4d0fcfd4af2428f624b79379707e58b3
Jordan Leach <jordonleach@gmail.com>
2020-04-27 14:04:13 -0400

    init

---
+++ webapp/src/components/sshReader.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    {{ configString }}
+  </div>
+</template>
+
+<script>
+import SSHConfig from "ssh-config";
+
+const config = SSHConfig.parse(`
+    IdentityFile ~/.ssh/id_rsa
+  `);
+
+const configString = SSHConfig.stringify(config);
+
+
+
+export default {
+  name: "sshReader",
+  data: function() {
+    return {
+      configString: configString,
+    };
+  },
+};
+</script>
